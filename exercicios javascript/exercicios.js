4f933184af7c81789cf5a948d9fabbfb5eee228f
luishtcampos <luishtcampos7@gmail.com>
2022-06-23 11:44:41 -0300

    exercicio 3

--- exercicios javascript/exercicios.js
+++ exercicios javascript/exercicios.js
@@ -1,1 +1,9 @@
-adfas
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let mediaAritmetica = 0;
+for (let index = 0; index < numbers.length; index++) {
+   mediaAritmetica += numbers[index] / numbers.length;
+
+    
+    
+}
+console.log(mediaAritmetica);
