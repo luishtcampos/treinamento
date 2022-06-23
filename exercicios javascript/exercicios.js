d0e0b59b40551530a856d3c87a8f6e172b033940
luishtcampos <luishtcampos7@gmail.com>
2022-06-23 12:30:49 -0300

    exercicio 5

--- exercicios javascript/exercicios.js
+++ exercicios javascript/exercicios.js
@@ -1,9 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let mediaAritmetica = 0;
+// let mediaAritmetica = 0;
+let posicaoNumero = 0;
 for (let index = 0; index < numbers.length; index++) {
-   mediaAritmetica += numbers[index] / numbers.length;
+  // mediaAritmetica += numbers[index] / numbers.length;
+   if (numbers[index] > numbers[posicaoNumero]) {
+    posicaoNumero = index;
+    
+   }}
+console.log(posicaoNumero);
+    
+    
 
-    
-    
-}
-console.log(mediaAritmetica);
+//console.log(mediaAritmetica);
+//console.log('Valor maior que 20');
