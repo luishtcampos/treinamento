a51043f9436ace447081b51667c090ef0afd9a96
luishtcampos <luishtcampos7@gmail.com>
2022-06-24 18:07:43 -0300

    desenvolvimento de hoje
    exit

---
+++ exercicios javascript/bloco4exercicio.js
@@ -0,0 +1,74 @@
+/*let usuario = {
+    id: 99,
+    email: 'jakeperalta@gmail.com',
+    infoPessoal: {
+      nome: 'Jake',
+      sobrenome: 'Peralta',
+      endereco: {
+        rua: 'Smith Street',
+        bairro: 'Brooklyn',
+        cidade: 'Nova Iorque',
+        estado: 'Nova Iorque',
+      },
+    },
+  };
+  
+  console.log(usuario['id']); // 99
+  console.log(usuario.email); // jakeperalta@gmail.com
+  
+  console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+  console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque */
+
+ /* let moradores = [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ];
+  
+  let primeiroMorador = moradores[0];
+  console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+  console.log(primeiroMorador['andar']); // 10
+  
+  let ultimoMorador = moradores[moradores.length - 1];
+  console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+  console.log(ultimoMorador.nome); // Zoey*/
+  
+ /* let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };*/
+
+let player = {
+  name: 'Marta',
+  lastname: 'Silva',
+  age: 34,
+  medals:{
+    golden: 2,
+    silver: 3,
+  },
+  bestInTheWorld:[2006, 2007, 2008, 2009, 2010, 2018]
+  
+};
+console.log('a jogadora ' + player.name + ' ' + player.lastname + ' tem ' + player.age + ' anos de idade');
+console.log('a jogadora ' + player.name + + ' ' + player.lastname + 'foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes')
---
+++ exercicios javascript/exercicio-aula.js
@@ -0,0 +1,15 @@
+/* 1. Faça um algoritmo que calcule a soma de 35 a 105 usando a estrutura “for” e retorne no formato:
+A soma de 35 a 105 é: X. */
+
+
+
+let numeros = [];
+for (let index = 35; index <= 105; index += 1) {
+    numeros.push(index);
+         let soma = index + 1
+         console.log(numeros)
+    }
+
+    
+    
+}
